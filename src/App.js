d335f446d35ec713ad70219a6c7e6c889c106eef
LuizVJr2001 <you@example.com>
2023-04-01 18:52:10 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Video from "./pages/Video"
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app__videos'>
+        <Video 
+          likes = {111}
+          messages = {222}
+          shares = {333}
+          name = "Luiz"
+          description = "Brecker o goleiro"
+          music = "musica épica"
+          url = "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video 
+          likes = {444}
+          messages = {555}
+          shares = {666}
+          name = "Lorena"
+          description = "Bird olhando para a camera"
+          music = "Clap your hands"
+          url = "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
